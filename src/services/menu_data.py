351d67d68c2caf81a8be06bfe0bf68fdb5d23dfc
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-18 17:24:04 -0300

    Requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,35 @@
+import csv
+from models.dish import Dish  # noqa: F401, E261, E501
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = set()
+        self.generate_menu()
+
+    def read(self):
+        with open(self.source_path, encoding="utf-8") as file:
+            data = csv.DictReader(file)
+            return list(data)
+
+    def generate_menu(self):
+        data = self.read()
+        dish_dict = {}
+
+        for ingredient in data:
+            dish_name = ingredient["dish"]
+            dish_price = float(ingredient["price"])
+            ingredient_name = ingredient["ingredient"]
+            ingredient_amount = int(ingredient["recipe_amount"])
+
+            if dish_name not in dish_dict:
+                dish_dict[dish_name] = Dish(dish_name, dish_price)
+
+            dish_dict[dish_name].add_ingredient_dependency(
+                Ingredient(ingredient_name), ingredient_amount
+            )
+
+        self.dishes.update(dish_dict.values())
