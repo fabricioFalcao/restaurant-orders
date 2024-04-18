3cb2a3315a7dceda28d6ac52755a87f80de5d352
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-18 12:27:37 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,29 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
+from src.models.ingredient import Ingredient, Restriction
 
 # Req 1
 def test_ingredient():
-    pass
+    salmon = Ingredient('salmão')
+    salmon2 = Ingredient('salmão')
+    bacon = Ingredient('bacon')
+
+    # The class can be correctly instantiated
+    assert salmon.name == 'salmão'
+
+
+    # Test the custom __repr__ method
+    assert repr(salmon) == "Ingredient('salmão')"
+
+    # Test the custom __eq__ method
+    assert salmon == salmon2
+
+    # Test the custom __hash__ method
+    assert hash(salmon) == hash(salmon2)
+    assert hash(salmon) != hash(bacon)
+
+    # The restrictions are correctly set (order-independent comparison)
+    assert salmon.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
+
